4dfae456f4e0331603e4e734e40b6c31bfe51aea
Eric Burden <eric.w.burden@gmail.com>
2023-12-03 08:12:02 -0500

    Cleanup, part 1

--- src/main/kotlin/dev/ericburden/aoc2023/Day03.kt
+++ src/main/kotlin/dev/ericburden/aoc2023/Day03.kt
@@ -2,6 +2,10 @@ package dev.ericburden.aoc2023
 
 import kotlin.collections.mutableMapOf
 
+// There are two possible items on the schematic, either a number that
+// may span multiple grid spaces, or a non-numeric character called a
+// 'symbol'. Each number needs to be uniquely identified, so we can
+// numbers with duplicate values in the schematic representation below.
 sealed class SchematicItem {
   data class Number(val id: Int, val number: Int) : SchematicItem()
   data class Symbol(val char: Char) : SchematicItem()
@@ -9,39 +13,52 @@ sealed class SchematicItem {
 
 typealias Coordinate = Pair<Int, Int>
 
-data class EngineSchematic(
+/**
+ * This class represents the engine schematic
+ *
+ * @property map A mapping of coordinates to schematic items
+ * @property numbers A list allowing for easy iteration over numeric schematic items
+ * @property symbols A list allowing for easy iteration over symbol schematic items
+ */
+data class EngineSchematic
+private constructor(
     val map: MutableMap<Coordinate, SchematicItem>,
-    val numbers: MutableMap<SchematicItem.Number, List<Coordinate>>,
+    val numbers: List<Pair<SchematicItem.Number, List<Coordinate>>>,
     val symbols: List<Pair<SchematicItem.Symbol, Coordinate>>
 ) {
   companion object {
+    /**
+     * This factory function serves as the constructor
+     *
+     * Parses an [EngineSchematic] from the input string
+     *
+     * @param input The input string
+     * @return An [EngineSchematic] represented by the input string
+     */
     fun fromString(input: String): EngineSchematic {
       val map = mutableMapOf<Coordinate, SchematicItem>()
-      val numbers = mutableMapOf<SchematicItem.Number, List<Coordinate>>()
+      val numbers = mutableListOf<Pair<SchematicItem.Number, List<Coordinate>>>()
       val symbols = mutableListOf<Pair<SchematicItem.Symbol, Coordinate>>()
 
+      // Iterate over the lines with support for skipping forward when
+      // a multi-digit number is encountered.
       for ((row, line) in input.trimEnd().split("\n").withIndex()) {
-        // Iterate over the line with support for skipping forward when
-        // a multi-digit number is encountered
         var col = 0
         while (col < line.length) {
-          var current_char = line[col]
-
-          if (current_char == '.') {
+          if (line[col] == '.') {
             // If the current character is a period, skip this column
             col += 1
-          } else if (current_char.isDigit()) {
+          } else if (line[col].isDigit()) {
             // If the current character is numeric digit, parse the number,
             // add it to the [map], and skip ahead to the end of the
             // number string
             var buffer = StringBuilder()
             val first_col = col
             val id = (row * line.length) + col // The ID for this number
-            while (current_char.isDigit()) {
-              buffer.append(current_char)
+            while (line[col].isDigit()) {
+              buffer.append(line[col])
               col += 1
               if (col >= line.length) break // Safety measure
-              current_char = line[col]
             }
 
             // No need to check the string, we guaranteed it only contains digits
@@ -52,7 +69,7 @@ data class EngineSchematic(
               map.put(coordinate, partNumber)
               numberCoords.add(coordinate)
             }
-            numbers.put(partNumber, numberCoords)
+            numbers.add(partNumber to numberCoords)
           } else {
             // Otherwise, we're dealing with a symbol.
             val symbol = SchematicItem.Symbol(line[col])
@@ -124,7 +141,7 @@ data class EngineSchematic(
 
 class Day03(input: String) {
 
-  private val schematic = EngineSchematic.Companion.fromString(input)
+  private val schematic = EngineSchematic.fromString(input)
 
   fun solvePart1(): Int = schematic.partNumbers().sumOf { it.number }
 
