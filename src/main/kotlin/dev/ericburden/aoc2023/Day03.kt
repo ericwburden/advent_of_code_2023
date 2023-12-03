fa3e959ab79f330d4226732b142b686614af4572
Eric Burden <eric.w.burden@gmail.com>
2023-12-03 08:47:14 -0500

    Finish cleaning up the code for this solution.

--- src/main/kotlin/dev/ericburden/aoc2023/Day03.kt
+++ src/main/kotlin/dev/ericburden/aoc2023/Day03.kt
@@ -65,8 +65,8 @@ private constructor(
               // We'll also keep a list of numbers and the coordinates associated with
               // the digits of that number.
               var numberCoords = mutableListOf<Coordinate>()
-              for (inner_col in first_col until col) {
-                val coordinate = Coordinate(row, inner_col)
+              for (digit_col in first_col until col) {
+                val coordinate = Coordinate(row, digit_col)
                 map.put(coordinate, partNumber)
                 numberCoords.add(coordinate)
               }
@@ -90,17 +90,35 @@ private constructor(
     }
   }
 
+  /**
+   * Extract the true part numbers from the schematic
+   *
+   * Just because there's a number in the schematic doesn't mean it's a _part_ number. Which is
+   * weird. A real part number is adjacent to a symbol. This function checks around each number for
+   * a symbol and returns all [SchematicItem.Number]s with an adjacent symbol.
+   *
+   * @return A list of [SchematicItem.Number] objects
+   */
   fun partNumbers(): List<SchematicItem.Number> {
     val offsets = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
     val partNumbers = mutableListOf<SchematicItem.Number>()
 
+    // For each number/list of coordinates pair in the [numbers] list,
+    // check all around the number for a symbol. Technically, we're
+    // re-checking several indices for numbers with multiple digits.
+    // It's possible to keep track of coordinates checked, but I'm not
+    // convinced it's worth the extra effort.
     for ((number, coordinates) in numbers) {
       number@ for (coordinate in coordinates) {
         for (offset in offsets) {
+          // Get the neighboring coordinate to check
           val row = coordinate.first + offset.first
           val col = coordinate.second + offset.second
           val checkAt = Coordinate(row, col)
 
+          // If there's a value at that coordinate and that value is
+          // a [SchematicItem.Symbol], then this number really is
+          // a part number! Add it to our list and move on.
           val maybeSymbol = map.get(checkAt) as? SchematicItem.Symbol
           if (maybeSymbol != null) {
             partNumbers.add(number)
@@ -113,30 +131,53 @@ private constructor(
     return partNumbers
   }
 
+  /**
+   * Calculate and return all gear ratios in the schematic
+   *
+   * Some symbols are gears. Each gear is associated with two numbers, and the 'gear ratio' is the
+   * product of those two numbers.
+   *
+   * @return A list of all gear ratios represeted on the schematic
+   */
   fun gearRatios(): List<Int> {
     val offsets = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
     val ratios = mutableListOf<Int>()
 
+    // For each symbol/coordinate pair in the symbols list, check around that
+    // symbol for exactly two numbers. If two numbers are found, add their
+    // product to the gear ratio list.
     symbol@ for ((symbol, coordinate) in symbols) {
       if (symbol.char != '*') continue // skip it, not a gear
-      var ratio = 1
-      var numbersSeen = mutableSetOf<SchematicItem.Number>()
-      var numbersAdded = 0
 
+      // We _do_ need to keep track of which numbers we've already seen,
+      // since each number can have multiple digits and the symbol may
+      // be adjacent to more than one of them.
+      var numbersSeen = mutableSetOf<Int>()
+      var numbersAdded = 0
+      var ratio = 1
+
+      // Check all around the symbol...
       for (offset in offsets) {
         val row = coordinate.first + offset.first
         val col = coordinate.second + offset.second
         val checkAt = Coordinate(row, col)
-        val maybeNumber = map.get(checkAt) as? SchematicItem.Number
 
-        if (maybeNumber != null && !numbersSeen.contains(maybeNumber)) {
+        // If the neighboring coordinate contains a value that can be
+        // coerced to a [SchematicItem.Number], we haven't added that
+        // number to the gear ratio yet, and it's not the third (or
+        // greater) number we've found, then we add (by multiplying) that
+        // number to the gear ratio and add the number's ID to the set
+        // of seen numbers.
+        val maybeNumber = map.get(checkAt) as? SchematicItem.Number
+        if (maybeNumber != null && !numbersSeen.contains(maybeNumber.id)) {
           if (numbersAdded >= 2) continue@symbol // more than two numbers
           ratio *= maybeNumber.number
-          numbersSeen.add(maybeNumber)
+          numbersSeen.add(maybeNumber.id)
           numbersAdded += 1
         }
       }
 
+      // Only add the raio to ratios if we saw two numbers
       if (numbersAdded == 2) ratios.add(ratio)
     }
 
@@ -146,9 +187,14 @@ private constructor(
 
 class Day03(input: String) {
 
+  // The real work is done here
   private val schematic = EngineSchematic.fromString(input)
 
+  // In part one, we figure out which numbers really represent parts
+  // and return the sum of all of them.
   fun solvePart1(): Int = schematic.partNumbers().sumOf { it.number }
 
+  // In part two, we find the gears (which are a bit trickier) and return
+  // the sum of all the gear ratios.
   fun solvePart2(): Int = schematic.gearRatios().sum()
 }
